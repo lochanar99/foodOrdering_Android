5b4c60005605a1a81a2e61e323db50a37843557e
lochanar99 <lochana.r@yahoo.com>
2019-08-05 01:18:25 +0530

    Payament Commit3

--- app/src/main/java/com/example/order_food/Login.java
+++ app/src/main/java/com/example/order_food/Login.java
@@ -2,13 +2,35 @@ package com.example.order_food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
+
+    private ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        imageButton = findViewById(R.id.i1);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getMenuPage();
+            }
+        });
+    }
+
+    public void getMenuPage(){
+
+        Intent intent = new Intent(this,Menu.class);
+
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/order_food/Menu.java
+++ app/src/main/java/com/example/order_food/Menu.java
@@ -2,13 +2,16 @@ package com.example.order_food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 
 public class Menu extends AppCompatActivity implements View.OnClickListener{
 
     private RadioButton radioButton8, radioButton9, radioButton10;
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,23 @@ public class Menu extends AppCompatActivity implements View.OnClickListener{
         radioButton10 = findViewById(R.id.radioButton10);
         radioButton10.setOnClickListener(this);
 
+        b1 = (Button)findViewById(R.id.button2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getPayment();
+            }
+        });
+
+    }
+
+    public void getPayment(){
+
+        Intent intent = new Intent(this,Payments.class);
+
+        startActivity(intent);
     }
 
     @Override
