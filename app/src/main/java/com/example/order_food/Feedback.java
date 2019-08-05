054e04d440331eb2454bbe7b11e37e8f5f40b164
lochanar99 <lochana.r@yahoo.com>
2019-08-05 10:10:33 +0530

    Payament Commit3

--- app/src/main/java/com/example/order_food/Feedback.java
+++ app/src/main/java/com/example/order_food/Feedback.java
@@ -15,31 +15,33 @@ public class Feedback extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
-        //final EditText edit1 = (EditText)findViewById(R.id.editText);
-        //final EditText edit2 = (EditText)findViewById(R.id.editText2);
-        //Button btn=(Button) findViewById(R.id.button);
-        //btn.setOnClickListener (new View.OnClickListener() {
-
-
-        //@Override
-        //public void onClick(View view) {
-               /* Intent i = new Intent(Intent.ACTION_SEND);
-                i.setType("message/html");
-                i.putExtra(Intent.EXTRA_EMAIL,new String(original,"xyz@gmail.com"));
-                i.putExtra(Intent.EXTRA_SUBJECT,value,"Feedback From App");
-                i.putExtra(Intent.EXTRA_TEXT,value,"Name:"+edit1.getText()+"\n Message:"+edit2.getText());
-                try{
-                    startActivity(Intent.createChooser(i,title,"Please select Email"));
-                }
-                catch(android.content.ActivityNotFoundException ex)
-                {
-                    Toast.makeText(context.MainActivity.this, text,"There are no Email Clients" , Toast.LENGTH_SHORT).show();
-                }
-            }
+        /*
+        final EditText edit1 = (EditText)findViewById(R.id.editText);
+        final EditText edit2 = (EditText)findViewById(R.id.editText2);
+        Button btn=(Button) findViewById(R.id.button);
+        btn.setOnClickListener (new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.setType("message/html");
+        i.putExtra(Intent.EXTRA_EMAIL,new String(original,"xyz@gmail.com"));
+        i.putExtra(Intent.EXTRA_SUBJECT,value,"Feedback From App");
+        i.putExtra(Intent.EXTRA_TEXT,value,"Name:"+edit1.getText()+"\n Message:"+edit2.getText());
+        try{
+        startActivity(Intent.createChooser(i,title,"Please select Email"));
+        }
+        catch(android.content.ActivityNotFoundException ex)
+        {
+        Toast.makeText(context.MainActivity.this, text,"There are no Email Clients" , Toast.LENGTH_SHORT).show();
+        }
+        }
         }}
-};
+        };
 
-}*/
+        }
+        */
+
+
     }
 }
 
