34414cff058a2e5d3f1dd1f06b2ce689e00daec8
HusmithaSilva <husmitha.silva0@gmail.com>
2019-08-21 20:13:33 +0530

    initial menu commit

--- app/src/main/java/com/example/order_food/Menu.java
+++ app/src/main/java/com/example/order_food/Menu.java
@@ -8,9 +8,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 
-public class Menu extends AppCompatActivity implements View.OnClickListener{
+public class Menu extends AppCompatActivity {
 
-    private RadioButton radioButton8, radioButton9, radioButton10;
+
     private Button b1;
 
     @Override
@@ -18,12 +18,6 @@ public class Menu extends AppCompatActivity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        radioButton8 = findViewById(R.id.radioButton8);
-        radioButton8.setOnClickListener(this);
-        radioButton9 = findViewById(R.id.radioButton9);
-        radioButton9.setOnClickListener(this);
-        radioButton10 = findViewById(R.id.radioButton10);
-        radioButton10.setOnClickListener(this);
 
         b1 = (Button)findViewById(R.id.button2);
 
@@ -44,33 +38,9 @@ public class Menu extends AppCompatActivity implements View.OnClickListener{
         startActivity(intent);
     }
 
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.radioButton8:
-                if(radioButton8.isSelected()){
-                    radioButton8.setSelected(false);
-                    radioButton8.setSelected(true);
-                }else{
-                    radioButton8.setSelected(true);
-                    radioButton8.setSelected(true);
-                }
-                break;
-        }
-
-        switch (view.getId()){
-            case R.id.radioButton9:
-                if(radioButton9.isSelected()){
-                    radioButton9.setSelected(false);
-                    radioButton9.setSelected(true);
-                }else{
-                    radioButton9.setSelected(true);
-                    radioButton9.setSelected(true);
-                }
-                break;
-        }
 
 
 
     }
-}
+
+
