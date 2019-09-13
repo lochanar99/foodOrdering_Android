3de8ba780d370e0435fa0196dcce671ad662a27c
dileepapavel <it18107210@my.sliit.lk>
2019-09-13 15:24:52 +0530

    spalash activity was made

---
+++ app/src/main/java/com/example/order_food/spalash.java
@@ -0,0 +1,31 @@
+package com.example.order_food;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class spalash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalash);
+
+        //Removing Action Bar
+
+        getSupportActionBar().hide();
+
+        new Handler() .postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(spalash.this,MainActivity.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        }, 3000);
+    }
+}
