7f3a3f9d749a7d19607b49cf52a3d465b1bd513e
HusmithaSilva <husmitha.silva0@gmail.com>
2019-09-23 02:05:31 +0530

    menu retrieve commit

--- app/src/main/java/com/example/order_food/Menu.java
+++ app/src/main/java/com/example/order_food/Menu.java
@@ -18,8 +18,8 @@ import com.example.order_food.Database.DBHelper;
 public class Menu extends AppCompatActivity {
 
     TextView textView1;
-    EditText editText1, editText2;
-     Button b1, button9, button2;
+    EditText editText1, editText2, editText3, editText4, editText5, editText6, editText7, editText8;
+     Button b1, button9, button2, button3, button4;
      DBHelper db1;
 
     @Override
@@ -29,8 +29,20 @@ public class Menu extends AppCompatActivity {
 
         editText1 = findViewById(R.id.editTexthus2);
         editText2 = findViewById(R.id.editTexthus1);
+        editText3 = findViewById(R.id.editTexthus3);
+        editText4 = findViewById(R.id.editTexthus4);
+        editText5 = findViewById(R.id.editTexthus5);
+        editText6 = findViewById(R.id.editTexthus6);
+        editText7 = findViewById(R.id.editTexthus7);
+        editText8 = findViewById(R.id.editTexthus8);
+
         b1 = (Button) findViewById(R.id.button1);
         button9 =(Button) findViewById(R.id.buttonhusbtn1);
+        button2 = findViewById(R.id.buttonhusbtn2);
+        button3 = findViewById(R.id.buttonhusbtn3);
+        button4 = findViewById(R.id.buttonhusbtn4);
+
+
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +54,9 @@ public class Menu extends AppCompatActivity {
         });
 
         addMenuDetails();
+        addMenuDetails1();
+        addMenuDetails2();
+        addMenuDetails3();
         //viewmenuDetails();
     }
 
@@ -50,7 +65,7 @@ public class Menu extends AppCompatActivity {
         button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean isInserted = db1.addMenu(editText2.getText().toString(),
+                boolean isInserted = db1.addMenu(editText3.getText().toString(),
                         Integer.parseInt(editText1.getText().toString()));
 
                 if(isInserted = true){
@@ -62,6 +77,55 @@ public class Menu extends AppCompatActivity {
         });
 
     }
+
+    public void addMenuDetails1(){
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isInserted = db1.addMenu(editText2.getText().toString(),
+                        Integer.parseInt(editText4.getText().toString()));
+
+                if(isInserted = true){
+                    Toast.makeText(Menu.this, "Successfully added", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(Menu.this, "Not Added", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void addMenuDetails2(){
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isInserted = db1.addMenu(editText5.getText().toString(),
+                        Integer.parseInt(editText6.getText().toString()));
+
+                if(isInserted = true){
+                    Toast.makeText(Menu.this, "Successfully added", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(Menu.this, "Not Added", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    public void addMenuDetails3(){
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean isInserted = db1.addMenu(editText7.getText().toString(),
+                        Integer.parseInt(editText8.getText().toString()));
+
+                if(isInserted = true){
+                    Toast.makeText(Menu.this, "Successfully added", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(Menu.this, "Not Added", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 
    /* public void viewmenuDetails(){
         button2.setOnClickListener(new View.OnClickListener() {
